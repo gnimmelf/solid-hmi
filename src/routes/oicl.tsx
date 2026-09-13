56b4bc0ab3ec58f8d6994a561aa0612f2d3f9325
Flemming Hansen <gnimmelf@gmail.com>
2026-09-13 18:48:38 +0200

    Adding OICL Webcomponents Demo

---
+++ src/routes/oicl.tsx
@@ -0,0 +1,12 @@
+import { Title } from '@solidjs/meta';
+import Counter from '../components/Counter';
+import logo from '../logo.svg';
+
+export default function Home() {
+  return (
+    <main>
+      <Title>OICL - Webcomponents Demo</Title>
+      -- TBD! --
+    </main>
+  );
+}
